3e69ca8517a8981347084293d70234ec4339ccd2
Miriam Poletti <polettimiriam@gmail.com>
2022-10-20 18:27:08 +0200

    image effect on homepage

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,13 @@
 import './index.scss';
 import Navbar from '../../components/Navbar';
+import image from '../../images/img-01.png';
 
 const Home = () => {
     return <div className='container homepage'>
         <Navbar />
-        <div>
-            
+        <div className='img-container'>
+            <img className='work-img' src={image} alt='test' />
+            <img className='work2-img' src={image} alt='test' />
         </div>
     </div>
 }
