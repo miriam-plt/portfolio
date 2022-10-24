bb17416ab81bc42340c560ec73a75900d16be560
Miriam Poletti <polettimiriam@gmail.com>
2022-10-24 21:49:15 +0200

    working on homepage added horizontal scroll

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,14 +1,29 @@
 import './index.scss';
+import { useState } from 'react';
 import Navbar from '../../components/Navbar';
 import image from '../../images/img-01.png';
 
 const Home = () => {
+    const [isHovering, setIsHovering] = useState(false);
+
+    const handleMouseOver = () => {
+        setIsHovering(true);
+    };
+
+    const handleMouseOut = () => {
+        setIsHovering(false);
+    };
+
+
     return <div className='container homepage'>
         <Navbar />
         <div className='img-container'>
-            <img className='work-img' src={image} alt='test' />
-            <img className='disappear work2-img' src={image} alt='test' />
-            <img className='disappear work3-img' src={image} alt='test' />
+            <img className={isHovering ? 'img-hover' : 'img-unselected'} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} src={image} alt='test' />
+            <img className={isHovering ? 'img-hover' : 'img-unselected'} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} src={image} alt='test' />
+            <img className={isHovering ? 'img-hover' : 'img-unselected'} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} src={image} alt='test' />
+            <img className={isHovering ? 'img-hover' : 'img-unselected'} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} src={image} alt='test' />
+            <img className={isHovering ? 'img-hover' : 'img-unselected'} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} src={image} alt='test' />
+            <img className={isHovering ? 'img-hover' : 'img-unselected'} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} src={image} alt='test' />
         </div>
     </div>
 }
