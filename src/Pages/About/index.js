6bfdb1283834075d54e6becd0c0e723b1636347e
Miriam Poletti <polettimiriam@gmail.com>
2022-10-26 23:20:00 +0200

    added about page text

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -2,10 +2,18 @@ import './index.scss';
 import Navbar from '../../components/Navbar';
 
 const About = () => {
-    return <div className='container about-page'>
+    return <div>
         <Navbar />
         <div className='about-info'>
-            <h5>About page</h5>
+            <p>Miriam Poletti (b. 1991, pronouns she/they) lives and works in Berlin, Germany.</p>
+            <p>Her artistic practice is interdisciplinary and includes photography, sculpture, video and creative writing. 
+            Their work seeks to question notions of tenderness, fragility and sociality on both a communal and individual level, 
+            serving as an embodied methodology to create new models of social interaction. </p>
+            <p>The notion of trauma plays a generative and creative role in her production. 
+            The body, investigated in its minimal alterations, is soft, weak, porous and easily shaped or even bruised by the proximity of the others. 
+            The audience is invited to interact physically and viscerally with the works, squeezing or gently touching them, leaving their temporary 
+            imprint on them, drinking and consuming them, wearing them, making them their own. 
+            The skin becomes a site of possibility in which the nature of the encounter is established through the performative process of touching.</p>
         </div>
     </div>
 }
