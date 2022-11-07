dfd511238db703f2715bf01416b9b8bf5a92bbae
Miriam Poletti <polettimiriam@gmail.com>
2022-11-07 13:25:02 +0100

    filtered work by id style

--- src/Pages/WorkDetails/index.js
+++ src/Pages/WorkDetails/index.js
@@ -1,6 +1,7 @@
 import './index.scss';
 import Navbar from '../../components/Navbar';
 import { useParams } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import WorksData from '../../assets/data/works-data.json';
 
 
@@ -15,17 +16,21 @@ function WorkDetails() {
     
    return <div>
         <Navbar />
-        <p>Details page: {id}</p>
+        
+        <div className='work__detail'>
         {selectedWork.map(work => {
             return (
                 <div key={work._id}>
-                <h1>Title: {work.title}</h1>
-                <p>Category: {work.category}</p>
-                <p>Duration: {work.duration}</p>
-                <p>Description: {work.description}</p>
+                <img className='work__detail-img' src={work.image} alt='main' />
+                <h1 className='work__detail-title'>{work.title}</h1>
+                <p className='work__detail-basics'>{work.category}{work.measurement ? '  |  ' + work.measurement : ''} {work.material ? '  |  ' + work.material : ''}  |  {work.year}</p>
+                <p className='work__detail-description'>{work.description}</p>
+                {work.link && <NavLink className='work__detail-link' to={work.link}><p>LINK</p></NavLink>}
+                {work.credits && <div><p className='work__detail-credits'>{work.credits[0]}</p><p className='work__detail-credits'>{work.credits[1]}</p></div>}
                 </div>
             )
         })}
+        </div>
        
     </div>
 };
