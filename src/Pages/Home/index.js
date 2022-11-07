0649702282f7a16f69c58fef4630c1378f3789b1
Miriam Poletti <polettimiriam@gmail.com>
2022-11-07 18:57:27 +0100

    homepage background

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,10 +3,11 @@ import Navbar from '../../components/Navbar';
 
 
 const Home = () => {
-    return <div>
+    return (
+    <div className='homepage-background'>
         <Navbar />
-
+        
     </div>
-}
+)}
 
 export default Home;
